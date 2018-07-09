7e69099f78c7b48427bfbecad506b2bb4d0ddfaa
Sultan <sehrism@kl-12718.local>
2018-07-09 18:59:10 +0300

    changes in folders

--- README.md
+++
@@ -1,2 +0,0 @@
-# fMRI_Cognitive_States_Sultan2018
-This projects aims to classify come cognitive states based on fMRI data and signal processing features
--- shuffleRow.m
+++
@@ -1,6 +0,0 @@
-function [X_perm,y_perm,shuffledRow] = shuffleRow(X,y)
-
-[r c] = size(X);
-shuffledRow = randperm(r);
-X_perm = X(shuffledRow, :);
-y_perm = y(shuffledRow, :);
