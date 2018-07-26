7a1525f66d1df03635160bff7821b64d3963fdfe
Sultan <sehrism@kl-12718.local>
2018-07-26 23:36:40 +0300

    ..

--- Leave1out_PWM/classify_6_subject_L1O_PWM_optimize_range.m
+++ Leave1out_PWM/classify_6_subject_L1O_PWM_optimize_range.m
@@ -3,32 +3,36 @@ addpath ./Functions
 addpath ./Functions/Netlab
 addpath ./datasets
 addpath /Users/sehrism/Documents/datasets
+addpath ./Leave1out_PWM
+addpath ./test
+
+
 for subject=1:6
     switch(subject)
         case 1
             load('data-starplus-04799-v7.mat')
             single_subject_classification_test
-            acc1= LeaveOut_PWM(X,Y);
+            acc1= Old_LeaveOut_PWM(X,Y);
         case 2
             load('data-starplus-04820-v7.mat')
             single_subject_classification_test
-            acc2= LeaveOut_PWM(X,Y);
+            acc2= Old_LeaveOut_PWM(X,Y);
         case 3
             load('data-starplus-04847-v7.mat')
             single_subject_classification_test
-            acc3= LeaveOut_PWM(X,Y);
+            acc3= Old_LeaveOut_PWM(X,Y);
         case 4
             load('data-starplus-05675-v7.mat')
             single_subject_classification_test
-            acc4= LeaveOut_PWM(X,Y);
+            acc4= Old_LeaveOut_PWM(X,Y);
         case 5
             load('data-starplus-05680-v7.mat')
             single_subject_classification_test
-            acc5= LeaveOut_PWM(X,Y);
+            acc5= Old_LeaveOut_PWM(X,Y);
         case 6
             load('data-starplus-05710-v7.mat')
             single_subject_classification_test
-            acc6= LeaveOut_PWM(X,Y);
+            acc6= Old_LeaveOut_PWM(X,Y);
     end
 end
 
