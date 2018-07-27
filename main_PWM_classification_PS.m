02fc04af1dae98008d9f138b6120e4ad6c874a58
SultanMo <isultan_@hotmail.com>
2018-07-27 04:55:07 +0300

    Updates

--- main_PWM_classification_PS.m
+++ main_PWM_classification_PS.m
@@ -7,8 +7,8 @@ addpath ./Leave1out_PWM
 addpath ./test
 addpath D:\SSI\Project\Datasets\starplus
 
-load('data-starplus-04799-v7.mat')
+load('data-starplus-05710-v7.mat')
 single_subject_classification_test
-[PWM, acc, outcome]= Classify_LeaveOut_PWM_functions(X,Y);
+[outcome, accuracy1, accuracy2]= Classify_LeaveOut_PWM_functions(X,Y);
 % acc = Old_LeaveOut_PWM(X,Y);
 % [acc,outcome]= Generate_PWM(X,Y);
