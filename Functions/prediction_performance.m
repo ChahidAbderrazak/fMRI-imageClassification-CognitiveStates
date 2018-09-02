bc05eac8b340ece0dc5ca875450118371d5b88af
SultanMo <isultan_@hotmail.com>
2018-09-03 01:19:08 +0300

    Update function

--- Functions/prediction_performance.m
+++ Functions/prediction_performance.m
@@ -1,11 +1,11 @@
-function [accuracy,sensitivity,specificity,precision,gmean,f1score]=prediction_performance(ytrue, yfit)
+function [accuracy]=prediction_performance(ytrue, yfit)
  
  
 C=confusionmat(ytrue, yfit);
  
-sensitivity = C(2,2)/(C(2,1)+C(2,2))*100;
-specificity = C(1,1)/(C(1,1)+C(1,2))*100;
-precision = (C(2,2)/(C(2,2)+C(1,2)))*100;
+% sensitivity = C(2,2)/(C(2,1)+C(2,2))*100;
+% specificity = C(1,1)/(C(1,1)+C(1,2))*100;
+% precision = (C(2,2)/(C(2,2)+C(1,2)))*100;
 accuracy= (C(1,1)+C(2,2))/(C(1,1)+C(1,2)+C(2,1)+C(2,2))*100;
-gmean = (sqrt(sensitivity*specificity));
-f1score = (2*((precision*sensitivity)/(precision+sensitivity)));
+% gmean = (sqrt(sensitivity*specificity));
+% f1score = (2*((precision*sensitivity)/(precision+sensitivity)));
